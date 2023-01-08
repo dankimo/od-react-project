5a1d6fc18b8c259c1bf5da81f690415513f243b9
Dan Kimmorley <dankimmorley@gmail.com>
2023-01-08 16:54:06 +1000

    moved json to api handlers

--- pages/api/details.js
+++ pages/api/details.js
@@ -1,3 +1,29 @@
 export default function handler(req, res) {
-    res.status(200).send("HELLO")
+  res.status(200).json(
+    {
+      "pageName": "Details",
+      "userDetails": [
+        {
+          "name": "Thomas Rodriguez",
+          "description": "These is very adorable. I am loving this. Seomtimes this pictures just are beautiful!",
+          "icon": "/assets/user1.png"
+        },
+        {
+          "name": "Thomas Rodriguez",
+          "description": "These is very adorable. I am loving this. Seomtimes this pictures just are beautiful!",
+          "icon": "/assets/user2.png"
+        },
+        {
+          "name": "Thomas Rodriguez",
+          "description": "These is very adorable. I am loving this. Seomtimes this pictures just are beautiful!",
+          "icon": "/assets/user3.png"
+        },
+        {
+          "name": "Thomas Rodriguez",
+          "description": "These is very adorable. I am loving this. Seomtimes this pictures just are beautiful!",
+          "icon": "/assets/user4.png"
+        }
+      ]
+    }
+  )
 }
--- pages/api/gallery.js
+++ pages/api/gallery.js
@@ -1,3 +1,29 @@
 export default function handler(req, res) {
-    res.status(200).send("HELLO")
+  res.status(200).json(
+    {
+      "pageName": "Gallery",
+      "pictureDetails": [
+        {
+          "description": "a bloke on a beach holding a guitar at sunset"
+        },
+        {
+          "description": "a kitten on a fluffy bed"
+        },
+        {
+          "description": "a city skyline at dusk"
+        }
+      ],
+      "videoDetails": [
+        {
+          "description": "a bloke on a beach holding a guitar at sunset video"
+        },
+        {
+          "description": "a kitten on a fluffy bed video"
+        },
+        {
+          "description": "a city skyline at dusk video"
+        }
+      ]
+    }
+  )
 }
